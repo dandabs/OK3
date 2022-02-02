5c348fcbd83741e44aae734d47a93f4ec77ffda6
Daniel Adams <danstanswinxp@gmail.com>
2022-02-02 21:21:44 +0000

    Migrated to https://github.com/dandabs/OK4

--- OK3/Pages/Portfolio.cshtml.cs
+++ OK3/Pages/Portfolio.cshtml.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+using System.IO;
 
 namespace OK3.Pages
 {
@@ -19,7 +22,22 @@ namespace OK3.Pages
 
         public void OnGet()
         {
+            string files = Directory.GetParent(System.AppDomain.CurrentDomain.BaseDirectory).FullName + "/library/projects";
+            Console.WriteLine(files);
+            if (Directory.Exists(files)) {
+                DirectoryInfo di = new DirectoryInfo(files);
+                foreach (FileInfo fi in di.GetFiles()) {
+                    Console.WriteLine(fi.FullName);
+                    using (StreamReader r = new StreamReader(fi.FullName)) {
+                        string json = r.ReadToEnd();
+                        dynamic project = JsonConvert.DeserializeObject(json);
+                        Console.WriteLine(project.name);
+                        
+                        //Console.WriteLine(content.CssClass);
 
+                    }
+                }
+            }
         }
     }
 }
